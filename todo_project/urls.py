67809cd6c7f74bfbf3ea23c1bb4f50806d1ae928
Jay <anjay94@gmail.com>
2023-03-05 03:32:42 -0500

    ?

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo_app.urls'))
+    path('', include('todo_app.urls')),
+    #path('list/', include('todo_app.urls'))
 ]
